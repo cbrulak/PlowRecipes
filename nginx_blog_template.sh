e45277fa93a4bb8a13e8ac43f66464115f85613a
Chris Brulak <ccmbrulak@gmail.com>
2013-05-17 11:22:01 -0600

    change logging

--- nginx_blog_template.sh
+++ nginx_blog_template.sh
@@ -8,8 +8,9 @@ server {
   client_max_body_size 4G;
   
   keepalive_timeout 5;
-  access_log off;
-  error_log /srv/$APP_NAME/log/blog.error.log;
+  #access_log off;
+  access_log /srv/blog/$APP_NAME/log/blog.access.log;
+  error_log /srv/blog/$APP_NAME/log/blog.error.log;
 
   root /srv/blog/$APP_NAME/;
   
